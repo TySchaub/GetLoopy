9f3bc222a570ea97d693852bbdfbbafd717c5a32
tyschaub <schaubtj@mail.uc.edu>
2025-10-07 15:04:27 -0400

    Task 6 Done

--- src/Task6.java
+++ src/Task6.java
@@ -1,6 +1,11 @@
 public class Task6 {
     public static void main(String[] args)
     {
-
+        for (int i = 0; i < 5; i++) { // rows
+            for (int j = 5; j > i; j--) { // columns
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
